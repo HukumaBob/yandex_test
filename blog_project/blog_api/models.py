8d7480b0971b3a3e4014ec9b40f2daf2c50bda5c
Nikita <ldc.endoscopy@gmail.com>
2023-08-09 07:28:04 +0400

    add models

--- blog_project/blog_api/models.py
+++ blog_project/blog_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.owner.username
+
+
+class Post(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
