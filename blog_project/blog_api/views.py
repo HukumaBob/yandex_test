94d06cd1b944817166f647104d8ab88631b6ae4c
Nikita <ldc.endoscopy@gmail.com>
2023-08-09 07:31:30 +0400

    add views

--- blog_project/blog_api/views.py
+++ blog_project/blog_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Blog, Post
+from .serializers import BlogSerializer, PostSerializer
 
-# Create your views here.
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
